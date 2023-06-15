32ea01117a8dae0a42750a98f5ade30e828cc29e
Peter Reutemann <fracpete@gmail.com>
2023-06-15 12:26:24 +1200

    incremented version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
             "indexed-png-stats=indexed_png_stats.generate:sys_main",
         ]
     },
-    version="0.0.1",
+    version="0.0.2",
     author='Peter Reutemann',
     author_email='fracpete@waikato.ac.nz',
     install_requires=[
